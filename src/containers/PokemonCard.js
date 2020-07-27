57fb9779768c0924d73e67b9f02e3436d57d467a
paperlambda <revzankamarullah@gmail.com>
2020-07-28 01:04:29 +0700

    add pokemon list initial request

--- src/containers/PokemonCard.js
+++ src/containers/PokemonCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const PokemonCard = () => {
+const PokemonCard = ({ pokemon }) => {
+  const { name } = pokemon
   return (
     <div className="w-full flex mt-2">
       <div className="h-auto w-48 bg-gray-500 text-center flex-none rounded-l" />
       <div className="w-full border-r border-b border-t border-gray-400 bg-white rounded-r px-4 py-2 flex flex-col justify-between leading-normal">
         <div className="text-sm text-gray-700">#012</div>
-        <div className="font-bold text-xl">Butterfree</div>
+        <div className="font-bold text-xl capitalize mb-1">{name}</div>
         <div className="mb-1">
           <div className="text-xs text-gray-600">Type</div>
           <div className="text-sm">Flying</div>
@@ -24,6 +26,13 @@ const PokemonCard = () => {
       </div>
     </div>
   )
+}
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    url: PropTypes.string.isRequired
+  })
 }
 
 export default PokemonCard
--- src/containers/PokemonList.js
+++ src/containers/PokemonList.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Pokedex } from 'pokeapi-js-wrapper'
 import PokemonCard from '@/containers/PokemonCard'
 
 const PokemonList = () => {
+  const PokemonAPI = React.useMemo(() => new Pokedex(), [])
+  const [pokemons, setPokemons] = React.useState(null)
+  const [isLoading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    _getPokemonList()
+  }, [])
+
+  const _getPokemonList = async () => {
+    try {
+      setLoading(true)
+      const pokemonList = await PokemonAPI.getPokemonsList({
+        limit: 10,
+        offset: 1
+      })
+
+      const { results } = pokemonList
+      setPokemons(results)
+      setLoading(false)
+    } catch (e) {
+      setLoading(false)
+      console.error(e)
+    }
+  }
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
-      {[1, 2, 3, 4, 5, 6].map(v => (
-        <PokemonCard key={v} />
+      {pokemons.map(pokemon => (
+        <PokemonCard key={pokemon.name} pokemon={pokemon} />
       ))}
     </div>
   )
